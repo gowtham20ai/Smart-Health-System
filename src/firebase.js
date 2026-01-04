dc8026738be20ac860fe666a624c0b434761b8e4
gowtham20ai <gowthamboominathan2005@gmail.com>
2026-01-04 12:19:07 -0800

    second

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,19 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1Sef1tVSVdGOLYeokSvLmT2weCjOQPn8",
-  authDomain: "healthcare-dde0f.firebaseapp.com",
-  databaseURL: "https://healthcare-dde0f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "healthcare-dde0f",
-  storageBucket: "healthcare-dde0f.firebasestorage.app",
-  messagingSenderId: "741531207902",
-  appId: "1:741531207902:web:1a94038fbac91e0377da4f"
+  apiKey: "AIzaSyBaP9N1kexbYfh9x7sY79qi95IpJ-L1MfY",
+  authDomain: "smart-health-system-b67dc.firebaseapp.com",
+  databaseURL: "https://smart-health-system-b67dc-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "smart-health-system-b67dc",
+  storageBucket: "smart-health-system-b67dc.firebasestorage.app",
+  messagingSenderId: "762061996406",
+  appId: "1:762061996406:web:74c10043bcccca487b3a57",
+  measurementId: "G-QL6RKMBP1Z"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+
+
+
